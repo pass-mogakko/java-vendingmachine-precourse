31d26c91965bb1bc05574e8c8d177f9599f785ef
송민진 <m_6595@naver.com>
2022-12-12 23:57:19 +0900

    docs(vendingmachine): 일치하는 상품명이 없을 경우 예외처리

--- src/main/java/vendingmachine/util/ExceptionHandler.java
+++ src/main/java/vendingmachine/util/ExceptionHandler.java
@@ -15,22 +15,4 @@ public class ExceptionHandler {
         }
     }
 
-    public static void handleAllException(Runnable doAction) {
-        try {
-            doAction.run();
-        } catch (IllegalArgumentException e) {
-            OutputView.printError(e);
-            handleAllException(doAction);
-        }
-    }
-
-    public static <T> T repeatWhenExceptionOccurs(Supplier<T> doAction) {
-        try {
-            return doAction.get();
-        } catch (IllegalArgumentException e) {
-            OutputView.printError(e);
-            return repeatWhenExceptionOccurs(doAction);
-        }
-    }
-
 }
